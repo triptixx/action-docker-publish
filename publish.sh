a6345c61c61de8f9bc0518c96de7dc445fb1a50e
Triptixx <54292597+triptixx@users.noreply.github.com>
2019-09-30 19:25:34 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 set -eo pipefail
 
-export TMP="$(jq --raw-output . "$GITHUB_EVENT_PATH")"
-echo "$TMP"
+#source tags.sh
+
+if [ "$1" = '--tags' ]; then
+    >&2 echo -e 'Running in --tags test mode'
+    shift
+    printf '%s\n' "$@" | parse_tags | xargs -n 1 | sort -u
+    exit 0
+fi
+
+if echo "$(jq --raw-output .head_commit.message "$GITHUB_EVENT_PATH")" | grep -qiF -e '[PUBLISH SKIP]' -e '[SKIP PUBLISH]'; then
+    >&2 echo -e 'Skipping publish'
+    exit 0
+fi
