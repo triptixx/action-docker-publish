05c0e06bc0dfe53796bcc51e31e5c1002f85cda3
Triptixx <54292597+triptixx@users.noreply.github.com>
2019-09-30 19:37:46 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -14,3 +14,16 @@ if echo "$(jq --raw-output .head_commit.message "$GITHUB_EVENT_PATH")" | grep -q
     >&2 echo -e 'Skipping publish'
     exit 0
 fi
+
+# $PLUGIN_FROM  re-tag from this repo
+# $PLUGIN_REPO  tag to this repo/repo to push to
+# $PLUGIN_TAGS  newline or comma separated list of tags to push images with
+
+USERNAME="${DOCKER_USERNAME:-${INPUT_USERNAME}}"
+PASSWORD="${DOCKER_PASSWORD:-${INPUT_PASSWORD}}"
+
+if [ -z "${USERNAME}" ]; then
+    error "Missing required docker 'username' for pushing"
+elif [ -z "${PASSWORD}" ]; then
+    error "Missing required docker 'username' for pushing"
+fi
